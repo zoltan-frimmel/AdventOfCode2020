a0e4864d400f5a023ac05ef801dde034d88381ae
Zoltan Frimmel <zoltan.x.frimmel@external.epiroc.com>
2020-12-02 12:34:03 +0100

    Setup.

---
+++ AdventOfCode2020/Util/IO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    public static class IO
+    {
+        public static IEnumerable<string> GetLines(string fn)
+        {
+            using (var reader = new StreamReader(File.OpenRead(fn)))
+            {
+                do
+                {
+                    var line = reader.ReadLine();
+
+                    if (line is null)
+                        break;
+
+                    yield return line;
+                } while (true);
+            }
+        }
+    }
+}
