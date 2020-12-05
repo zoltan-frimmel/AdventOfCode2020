b884e5b79b2664ef51a3fdac5c20d8dc6e1b7308
Zoltan Frimmel <zoltan.x.frimmel@external.epiroc.com>
2020-12-05 08:41:06 +0100

    Day5

---
+++ AdventOfCode2020/Day5.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public static class Day5
+    {
+        // So we are parsing binary numbers with MSB left-to-right.
+        // F = 0, B = 1, L = 0, R = 1
+        // Format XXXXXXX YYY
+
+        public const string Input = @".\inputs\day5.txt";
+
+        public static int PartOne()
+        {
+            return IO.GetLines(Input)
+                .Select(Parse)
+                .Select(seat => seat.Id)
+                .OrderByDescending(id => id)
+                .First();
+        }
+
+        public static int PartTwo()
+        {
+            var oids = IO.GetLines(Input)
+                .Select(Parse)
+                .Select(seat => seat.Id)
+                .OrderBy(id => id)
+                .ToArray();
+
+            return oids
+                .Zip(
+                    oids.Skip(1),
+                    (p, n) => n - p == 2 ? p + 1 : -1)
+                .Single(id => id > -1);
+        }
+
+        public static Seat Parse(string s)
+        {
+            if (s.Length != 10)
+                throw new ArgumentException($"Invalid seat id length: {s.Length}.");
+
+            var id = 0;
+            for (var i = 0; i < 7; i++)
+            {
+                if (s[i] == 'B')
+                    id |= (1 << 9 - i);
+            }
+
+            for (var i = 0; i < 3; i++)
+            {
+                if (s[7 + i] == 'R')
+                    id |= (1 << 2 - i);
+            }
+
+            return new Seat(id);
+        }
+
+        public struct Seat
+        {
+            public const int RowMask = 0b1111111000;
+            public const int ColMask = 0b0000000111;
+
+            public Seat(int id)
+            {
+                Id = id;
+            }
+
+            public int Id { get; }
+
+            public int Row => (Id & RowMask) >> 3;
+            public int Col => Id & ColMask;
+            
+
+            /// <inheritdoc />
+            public override string ToString()
+            {
+                return $"[Seat - Row: {Row}, Col: {Col}, ID: {Id}]";
+            }
+        }
+    }
+}
---
+++ AdventOfCode2020Test/Day5Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode2020;
+using Xunit;
+
+namespace AdventOfCode2020Test
+{
+    public class Day5Test
+    {
+        public static IEnumerable<object[]> PartOneTestData()
+        {
+            var seats = new[]
+            {
+                new ExpectedSeat {Row = 70, Col  = 7, Id = 567},
+                new ExpectedSeat {Row = 14, Col  = 7, Id = 119},
+                new ExpectedSeat {Row = 102, Col = 4, Id = 820}
+            };
+
+            var passes = new[]
+            {
+                "BFFFBBFRRR",
+                "FFFBBBFRRR",
+                "BBFFBBFRLL"
+            };
+
+            for (var i = 0; i < 3; i++)
+                yield return new object[] {passes[i], seats[i]};
+        }
+
+        public class ExpectedSeat
+        {
+            public int Row { get; set; }
+            public int Col { get; set; }
+            public int Id { get; set; }
+        }
+
+        [Theory]
+        [MemberData(nameof(PartOneTestData))]
+        public void PartOne(string boardingPass, ExpectedSeat expected)
+        {
+            var actual = Day5.Parse(boardingPass);
+
+            Assert.Equal(expected.Row, actual.Row);
+            Assert.Equal(expected.Col, actual.Col);
+            Assert.Equal(expected.Id, actual.Id);
+        }
+    }
+}
