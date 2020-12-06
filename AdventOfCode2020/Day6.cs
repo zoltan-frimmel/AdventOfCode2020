f32456fbfc1926f5a12cf58ea53eb9ed4647ceb0
Zoltan Frimmel <zoltan.x.frimmel@external.epiroc.com>
2020-12-06 11:58:36 +0100

    Day6

---
+++ AdventOfCode2020/Day6.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public static class Day6
+    {
+        public const string Input = @".\inputs\day6.txt";
+        public const string TestInput = @".\inputs\day6_testinput.txt";
+
+        public static int PartOne()
+        {
+            using (var reader = new StreamReader(Input))
+            {
+                return PartOne(reader);
+            }
+        }
+
+        public static int PartOne(TextReader reader)
+        {
+            return GetGroups(reader)
+                .Select(group => group.SelectMany(s => s.ToCharArray()))
+                .Select(group => group.Distinct().Count())
+                .Sum();
+        }
+
+        public static int PartTwo()
+        {
+            using (var reader = new StreamReader(Input))
+            {
+                return PartTwo(reader);
+            }
+        }
+
+        public static int PartTwo(TextReader reader)
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            return GetGroups(reader)
+                .Select(group => group
+                    .Aggregate(alphabet, (acc, p) => acc
+                        .Intersect(p)
+                        .ToArray()))
+                .Sum(group => group.Length);
+        }
+
+        public static IEnumerable<List<string>> GetGroups(TextReader reader)
+        {
+            var persons = new List<string>();
+
+            while (true)
+            {
+                var person = reader.ReadLine();
+
+                if (string.IsNullOrEmpty(person))
+                {
+                    yield return persons;
+
+                    persons = new List<string>();
+
+                    if (person == null)
+                    {
+                        yield break;
+                    }
+                }
+                else
+                {
+                    persons.Add(person);
+                }
+            }
+        }
+        
+    }
+}
---
+++ AdventOfCode2020Test/Day6Test.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using AdventOfCode2020;
+using Xunit;
+
+namespace AdventOfCode2020Test
+{
+    public class Day6Test
+    {
+        [Fact]
+        public void TestPartOne()
+        {
+            using (var reader = new StreamReader(File.OpenRead(Day6.TestInput)))
+            {
+                Assert.Equal(11, Day6.PartOne(reader));
+            }
+        }
+
+        [Fact]
+        public void TestPartTwo()
+        {
+            using (var reader = new StreamReader(File.OpenRead(Day6.TestInput)))
+            {
+                Assert.Equal(6, Day6.PartTwo(reader));
+            }
+        }
+    }
+}
